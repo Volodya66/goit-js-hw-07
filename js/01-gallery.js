4ea603fc97a37772cad797d793a21089b4ffe11a
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-08-22 12:06:57 +0300

    fixed galery
    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+
+const galleryCreate = galleryItems.map(({ preview:small, original:large, description }) =>
+ `
+  <li class="gallery__item" >
+        <a class="gallery__link" href="${large}">
+          <img
+          src="${small}" 
+          data-photo-name="${large}"
+          height="240"
+          alt="${description}">
+          </a>
+        </img>
+      </li>
+`
+).join(''); 
+
+gallery.insertAdjacentHTML('afterbegin',galleryCreate);
+
+
+const onClickPhoto = (evt) => {
+
+    if (evt.target === evt.currentTarget) {
+        return
+    };
+    
+    evt.preventDefault();
+    //?  ID фото по якому клікнули 
+    const dataAttributeImage = evt.target.dataset.photoName;
+    
+
+    console.log(evt.target);
+    console.log(dataAttributeImage);
+
+    
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+          <img
+          src="${dataAttributeImage}" 
+          alt="${galleryItems.description}">
+        </img>
+      
+    </div>
+    `)
+
+instance.show()
+};
+
+gallery.addEventListener('click', onClickPhoto);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+const galleryCreate = galleryItems.map(({ preview:small, original:large, description }) =>
+ `
+<li class="gallery__item ">
+      <a class="gallery__link " href="${large}">
+      <img class="gallery__image"
+           src="${small}"
+           alt="${description}" />
+      </a>
+</li>
+`
+).join(''); 
+
+gallery.insertAdjacentHTML('afterbegin', galleryCreate);
+
+
+let lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt'});
